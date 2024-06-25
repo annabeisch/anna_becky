d59456807d959932e9ae29524bdab46460d48a28
annabeisch <banana5525@icloud.com>
2024-06-25 10:05:02 -0700

    Initial Commit

---
+++ anna_becky/anna_beckyApp.swift
@@ -0,0 +1,17 @@
+//
+//  anna_beckyApp.swift
+//  anna_becky
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct anna_beckyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
